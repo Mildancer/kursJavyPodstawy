3ea35d1a3b918125c7e6c4170c5b70fddd105409
milosz nawrat <milosznawrat@gmail.com>
2018-12-15 12:53:47 +0100

    podstawy

---
+++ src/_2_typy/TypyObiektowe.java
@@ -0,0 +1,62 @@
+package _2_typy;
+
+import java.util.Scanner;
+
+public class TypyObiektowe {
+
+    public static void main(String[] args) {
+
+        System.out.print("Podaj swoje imie: "); // bez ln - nie robi entera
+
+        Scanner wejscie = new Scanner(System.in);
+
+        String imie = wejscie.nextLine(); // bedzie czytal znaki z klawiatury dopoki nie uzyje sie entera
+
+        // String imie = "Milosz";
+
+        int iloscZnakow = imie.length();
+
+
+        System.out.println(imie + " ma " + iloscZnakow + " znakow");
+
+      /*  if (warunek){
+
+    } else  {}
+    */
+
+
+
+        if (iloscZnakow <= 6) {
+            System.out.println("Krotkie slowo");
+        } else {
+            System.out.println("Dlugie slowo");
+        }
+
+        boolean endsWith =  imie.endsWith("a") || imie.endsWith("A");
+        // boolean endsWith =  imie.toLowerCase().endsWith("a") - imie przyrownuje do malych liter
+
+
+        if (imie.equalsIgnoreCase("Kuba")) {   // if (endsWith ==true) && !imie.equalsIgnoreCase("Kuba"))
+            System.out.println("meskie");
+        }
+        else if (endsWith == true){
+            System.out.println("damskie");
+        }
+        else
+            System.out.println("meskie");
+
+
+
+        /* inna propozycja: char znak= imie.charAt(iloscZnakow-1);
+        if (znak == 'a' || znak== 'A'
+        {
+            System.out.println("damskie");
+        }
+        else
+            System.out.println("meskie");
+
+
+         */
+    }
+    }
+
---
+++ src/_2_typy/TypyPrimitywne.java
@@ -0,0 +1,13 @@
+package _2_typy;
+
+public class TypyPrimitywne {
+
+    public static void main(String[] args) {
+    int iloscUczestnikow = 8;
+
+        System.out.println("Ilosc uczestnikow to:" + iloscUczestnikow);
+
+    }
+
+
+}
