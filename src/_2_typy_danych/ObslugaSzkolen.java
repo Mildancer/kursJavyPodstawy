b5be28826bb97005dfea770cff5c1aaaa7c6c3b6
milosz nawrat <milosznawrat@gmail.com>
2018-12-15 15:48:21 +0100

    Podstawy konktruktor

--- src/_2_typy_danych/ObslugaSzkolen.java
+++ src/_2_typy_danych/ObslugaSzkolen.java
@@ -1,4 +1,38 @@
 package _2_typy_danych;
 
+import java.time.LocalDate;
+import java.time.Month;
+
 public class ObslugaSzkolen {
+    public static void main(String[] args) {
+        Szkolenie szkolenieJava = new Szkolenie( "Szkolenie Java",  1000,  10,  LocalDate.of(2018, Month.DECEMBER, 15));
+
+        szkolenieJava.nazwa = "Szkolenie Java";
+        szkolenieJava.cena = 1000;
+        szkolenieJava.liczbaUczestnikow = 10;
+        szkolenieJava.data = LocalDate.of(2018, Month.DECEMBER, 15);
+
+        System.out.println("Szkolenie: " + szkolenieJava.nazwa);
+        System.out.printf("Cena: %.2f PLN", szkolenieJava.cena); // System.out.printf("\tCena: %.2f PLN\n" +szkolenieJava.cena)
+        System.out.println();
+        System.out.println("liczbaUczestnikow: " + szkolenieJava.liczbaUczestnikow);
+        System.out.println("data: " +szkolenieJava.data);
+        System.out.println();
+        System.out.println();
+
+        szkolenieJava.nazwa = "Szkolenie Python";
+        szkolenieJava.cena = 1300;
+        szkolenieJava.liczbaUczestnikow = 12;
+        szkolenieJava.data = LocalDate.of(2018, Month.DECEMBER, 15);
+
+        System.out.println("Szkolenie: " + szkolenieJava.nazwa);
+        System.out.printf("Cena: %.2f PLN", szkolenieJava.cena);
+        System.out.println();
+        System.out.println("liczbaUczestnikow: " +szkolenieJava.liczbaUczestnikow);
+        System.out.println("data: " +szkolenieJava.data);
+    }
+
+
+
+
 }
--- src/_2_typy_danych/Szkolenie.java
+++ src/_2_typy_danych/Szkolenie.java
@@ -1,10 +1,33 @@
-package _2_typy;
+package _2_typy_danych;
+
+import java.time.LocalDate;
 
 public class Szkolenie {
     double cena;
-    
+    int liczbaUczestnikow;
+    String nazwa;
+    LocalDate data;
 
+//1.tworzenie nowego obiektu
 
+    //kontruktor bezparametryczny- lista paramtrow w () pusta
 
+    public Szkolenie(){
+        System.out.println("Wykonal sie konstruktor bezparametryczny");  // dziala przy wywolaniu nowego obiektu
+    }
+
+    //kontruktor parametryczny -
+
+    public Szkolenie(String nazwa, int liczbaUczestnikow, double cena, LocalDate data){
+        System.out.println("Wykonal sie konstruktor parametryczny");  // dziala przy wywolaniu nowego obiektu
+        this.nazwa = nazwa; // konstruktor przypisuje swoja  do nazwy w fukcji -- wiadomo,ze nazwa odwoluje sie do nazwy  z kontruktora (pierwsza nazwa to nazwa funckji, druga konstruktora
+        this.liczbaUczestnikow = liczbaUczestnikow;
+        this.cena = cena;
+        this.data = data;
+    }
+
+    // przeciazenie nazwy fukcji
+
+//2.wypisanie  informacji  o obiekcie
 
 }
